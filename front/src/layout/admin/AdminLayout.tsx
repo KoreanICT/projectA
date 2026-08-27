77d1f4f975c56b3b98183eb337a05e8cc302fb0a
jyh928578@gmail.com <jyh928578@gmail.com>
2026-08-27 10:39:22 +0900

    admin 초안

---
+++ front/src/layout/admin/AdminLayout.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './adminLayout.module.css';
+import AdminSidebar from './AdminSidebar';
+
+interface AdminLayoutProps {
+    children: React.ReactNode;
+}
+
+const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
+    return (
+        <div className={styles.adminLayout}>
+
+            <header className={styles.header}>
+                <div className={styles.headerInner}>
+                    <div className={styles.logo}>
+                        관리자 페이지
+                    </div>
+
+                    <div className={styles.userMenu}>
+                        관리자
+                    </div>
+                </div>
+            </header>
+
+            <div className={styles.body}>
+
+                <AdminSidebar />
+
+                <main className={styles.main}>
+                    {children}
+                </main>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default AdminLayout;
--- front/src/router/AppRoutes.tsx
+++ front/src/router/AppRoutes.tsx
@@ -1,5 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+
 import Home from "../cont/Home";
+import AdminHome from "../cont/admin/AdminHome";
+
+import Layout from "../layout/Layout";
+import AdminLayout from "../layout/admin/AdminLayout";
 
 interface RouteItem {
     path: string;
@@ -9,18 +15,36 @@ interface RouteItem {
 }
 
 const AppRoutes: React.FC = () => {
-    const routeList = [
-        { path: '/', element: <Home /> },
-    ]
-    return (
-        <Routes>
-            {
-                routeList.map((route, idx) => (
-                    <Route key={idx} {...route} />
-                ))
-            }
-        </Routes>
-    )
-}
+    const { pathname } = useLocation();
 
-export default AppRoutes
+    const isManage = pathname.startsWith("/admin");
+
+    const routeList: RouteItem[] = [
+        { path: "/", element: <Home /> },
+        { path: "/admin", element: <AdminHome /> },
+    ];
+
+    return (
+        <>
+            {isManage ? (
+                <AdminLayout>
+                    <Routes>
+                        {routeList.map((route, idx) => (
+                            <Route key={idx} {...route} />
+                        ))}
+                    </Routes>
+                </AdminLayout>
+            ) : (
+                <Layout>
+                    <Routes>
+                        {routeList.map((route, idx) => (
+                            <Route key={idx} {...route} />
+                        ))}
+                    </Routes>
+                </Layout>
+            )}
+        </>
+    );
+};
+
+export default AppRoutes;
